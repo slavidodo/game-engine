207a234165282848855b1796c02ffd9ceae8807f
Omar Rehan <omar.se.rehan@gmail.com>
2020-06-10 07:06:37 +0200

    fixed physics scene termination bug

--- src/Engine/Physics/PhysicsTest.h
+++ src/Engine/Physics/PhysicsTest.h
@@ -4,33 +4,21 @@
 
 class PhysicsTester {
 public:
-    void EmptyGameLoop(PScene_ptr scene, float time);
+    void EmptyGameLoop(float time);
 
     #pragma region Tests
-    // Pointers, Destructors, & Shit
-    void Test0(PScene_ptr scene, float time);
-    // Capsule
-    void Test1(PScene_ptr scene, float time);
-     //Plane
-    void Test2(PScene_ptr scene, float time);
-    // Actor with multiple colliders
-    void Test3(PScene_ptr scene, float time);
-    // Convex Mesh
-    void Test4(PScene_ptr scene, float time);
-    // Triangle Mesh
-    void Test5(PScene_ptr scene, float time);
-    // Height Field
-    void Test6(PScene_ptr scene, float time);
-    // Falling sphere on boxes
-    void Test7(PScene_ptr scene, float time);
-    // Applying forces & torques
-    void Test8(PScene_ptr scene, float time);
-    // Collision filteration
-    void Test9(PScene_ptr scene, float time);
-    // Joints
-    void Test10(PScene_ptr scene, float time);
-    // Scene queries
-    void Test11(PScene_ptr scene);
+    void Test0(float time);
+    void Test1(float time);
+    void Test2(float time);
+    void Test3(float time);
+    void Test4(float time);
+    void Test5(float time);
+    void Test6(float time);
+    void Test7(float time);
+    void Test8(float time);
+    void Test9(float time);
+    void Test10(float time);
+    void Test11();
     #pragma endregion
 
     void RunTests();
