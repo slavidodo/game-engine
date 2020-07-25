1636d95106880164c2ac7198f4dfa79461f43479
Farid Ahmed <faridsharaf18@gmail.com>
2020-07-25 17:37:54 +0200

    add texture class

--- src/Engine/Core/Texture.cpp
+++ src/Engine/Core/Texture.cpp
@@ -1,10 +1,69 @@
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions
+// are met:
+//  * Redistributions of source code must retain the above copyright
+//    notice, this list of conditions and the following disclaimer.
+//  * Redistributions in binary form must reproduce the above copyright
+//    notice, this list of conditions and the following disclaimer in the
+//    documentation and/or other materials provided with the distribution.
+//  * Neither the name of NVIDIA CORPORATION nor the names of its
+//    contributors may be used to endorse or promote products derived
+//    from this software without specific prior written permission.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS ``AS IS'' AND ANY
+// EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+// IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+// PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL THE COPYRIGHT OWNER OR
+// CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+// EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+// PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+// PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY
+// OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+// (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+// OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
 
 #include "pch.h"
-
 #include "Texture.h"
 
-bool Texture::Load(uint8_t* buffer, size_t size)
-{
-	// TODO
-	return false;
+#define STB_IMAGE_IMPLEMENTATION
+#include "external/stbi_image.h"
+
+Texture::Texture() :mTextureID(0), mWidth(0), mHeight(0) {
+}
+
+bool Texture::Load(const std::string& fileName){
+	int nrChannels = 0;
+	int format = GL_RGB;
+	unsigned char* image = stbi_load(fileName.c_str(), &mWidth, &mHeight, &nrChannels, 0);
+
+	if (image == nullptr) {
+		std::cout << "Failed to load texture" << fileName.c_str() << std::endl;
+		return false;
+	}
+
+	if (nrChannels == 4) {
+		format = GL_RGBA;
+	}
+
+	glGenTextures(1, &mTextureID);
+	glBindTexture(GL_TEXTURE_2D, mTextureID);
+
+	glTexImage2D(mTextureID, 0, format, mWidth, mHeight, format, 0, GL_UNSIGNED_BYTE, image);
+	glGenerateMipmap(GL_TEXTURE_2D);
+
+	stbi_image_free(image);
+
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+
+	return true;
+}
+
+void Texture::Unload() {
+	glDeleteTextures(1, &mTextureID);
+}
+
+void Texture::Bind(){
+	glBindTexture(GL_TEXTURE_2D, mTextureID);
 }
--- src/Engine/Core/Texture.h
+++ src/Engine/Core/Texture.h
@@ -1,44 +1,52 @@
 
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions
+// are met:
+//  * Redistributions of source code must retain the above copyright
+//    notice, this list of conditions and the following disclaimer.
+//  * Redistributions in binary form must reproduce the above copyright
+//    notice, this list of conditions and the following disclaimer in the
+//    documentation and/or other materials provided with the distribution.
+//  * Neither the name of NVIDIA CORPORATION nor the names of its
+//    contributors may be used to endorse or promote products derived
+//    from this software without specific prior written permission.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS ``AS IS'' AND ANY
+// EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+// IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+// PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL THE COPYRIGHT OWNER OR
+// CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+// EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+// PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+// PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY
+// OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+// (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+// OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+
 #ifndef ENGINE_CORE_IMAGE_H
 #define ENGINE_CORE_IMAGE_H
 
 class Texture;
 typedef std::shared_ptr<Texture> Texture_ptr;
 
-enum textureType_t
-{
-	TEXTURE_TYPE_2D,
-	TEXTURE_TYPE_3D,
-	TEXTURE_TYPE_CUBE
-};
-
-enum textureWrapMode_t
-{
-	TEXTURE_WRAPMODE_CLAMP,
-	TEXTURE_WRAPMODE_REPEAT,
-	TEXTURE_WRAPMODE_MIRROR,
-};
-
-enum textureFilterMode_t
-{
-	TEXTURE_FILTERMODE_POINT,
-	TEXTURE_FILTERMODE_BILINEAR,
-	TEXTURE_FILTERMODE_TRILINEAR,
-};
-
-struct TextureSettings
-{
-	textureType_t type;
-	textureWrapMode_t wrapMode;
-	textureFilterMode_t filterMode;
-};
 
 class Texture
 {
+public:
+	Texture();
 protected:
-	bool Load(uint8_t* buffer, size_t size);
+	bool Load(const std::string& fileName);
+	void Unload();
 
+	void Bind();
+	int GetWidth() const{ return mWidth; }
+	int GetHeight() const { return mHeight; }
 private:
+	unsigned int mTextureID;
+	int mWidth;
+	int mHeight;
+
 	TextureSettings mTextureSettings;
 };
 
